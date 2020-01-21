1ce924b1556490e9a372e62264447aeeb9979337
Petra B <petrabrinck@icloud.com>
2020-01-21 19:28:33 +0100

    header in place, working on the api

--- code/src/App.js
+++ code/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+// import { BarcodeScanner } from './components/BarcodeScanner'
+import { ScanBarcode } from './components/ScanBarcode'
+// import { ProductList } from 'components/ProductList'
+import { Header } from 'components/header.js'
+
 
 export const App = () => {
   return (
     <div>
+      <Header />
+      {/* <BarcodeScanner /> */}
+      <ScanBarcode />
+
+      {/* <ProductList /> */}
       Find me in src/app.js!
     </div>
   )
---
+++ code/src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import 'components/header.css'
+// import { useDispatch } from 'react-redux'
+// import { TodoTasksSummary } from './TodoTasksSummary';
+// import { ClearButton } from './ClearButton';
+export const Header = () => {
+    // const dispatch = useDispatch()
+    return (
+        <div className="headerContainer">
+            {/* <!--Creating a hamburger menu with lines, made by div's--> */}
+            <div class="hamburger">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="headerInfo">
+                <h1>Nutrition info</h1>
+
+            </div>
+        </div>
+    )
+}
+
+
