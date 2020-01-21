1194fd6800379029686e2d60c537d1afefce0cdc
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-01-21 22:48:46 +0100

    changed the fetch to a reducer using asyncs actions with thunk and added a reducer for user interface ui and a component called Loadingindicator

--- code/src/App.js
+++ code/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { ui } from './ reducers/ui'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { ScanBarcode } from './components/ScanBarcode'
 import { Header } from './components/Header'
-import { ProductList } from 'components/ProductList'
+import { productFetch } from ' reducers/productFetch'
+import { LoadingIndicator } from 'components/LoadingIndicator'
+
+const reducer = combineReducers({
+  ui: ui.reducer,
+  productFetch : productFetch.reducer
+
+})
+
+export const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
+    <Provider store={store}>
+      <LoadingIndicator />
       <Header />
       <ScanBarcode />
       Find me in src/app.js!
-      {/* <ProductList /> */}
-    </div>
+    </Provider>
   )
 }
