b60d6081b5cd617a1ac3d09089387550f3f2e492
Agnes Ketesdi <agnes.ketesdi@gmail.com>
2020-01-23 22:12:57 +0100

    draft done

---
+++ code/src/components/Product.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export const Product = () => {
+  const product = useSelector(state => state.products.product);
+
+  if (!product) return null;
+
+  console.log("product", product);
+
+  return (
+    <>
+      <h1>Product:</h1>
+      <img src={product.product && product.product.image_nutrition_url} />
+      <h2>This product is: {product.product && product.product.labels}</h2>
+    </>
+  );
+};
---
+++ code/src/components/ScanBarcode.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { BarcodeScanner } from "components/BarcodeScanner";
+import { fetchProduct } from "reducers/products";
+import { useDispatch } from "react-redux";
+
+export const ScanBarcode = () => {
+  const [showScanner, setShowScanner] = useState(false);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      {!showScanner && (
+        <button type="button" onClick={() => setShowScanner(true)}>
+          Show scanner
+        </button>
+      )}
+
+      {showScanner && (
+        <BarcodeScanner
+          onDetected={code => {
+            console.log("Got barcode", code);
+            setShowScanner(false);
+            dispatch(fetchProduct(code));
+          }}
+        />
+      )}
+    </>
+  );
+};
