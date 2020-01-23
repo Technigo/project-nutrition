22c4ed4b321eca4f76ec0d778161a58d012a070a
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-01-23 20:50:55 +0100

    Update structure and style

--- code/src/components/BarcodeScanner.js
+++ code/src/components/BarcodeScanner.js
@@ -5,9 +5,10 @@ import styled from 'styled-components';
 const Camera = styled.div`
   width: 100%;
   height: 250px;
-  max-width: 500px;
+  max-width: 600px;
   box-shadow: 0 2px 4px 0 rgba(14, 30, 37, 0.4);
   border-radius: 10px;
+  padding: 0 1rem;
   overflow: hidden;
 `;
 
