e6a07fd4adb14e7c8bafe175e6e75b43715b83c4
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-01-23 17:15:18 +0100

    Update structure and style

--- code/src/components/ScanBarcode.js
+++ code/src/components/ScanBarcode.js
@@ -3,13 +3,22 @@ import { BarcodeScanner } from 'components/BarcodeScanner';
 import { useDispatch } from 'react-redux';
 import { fetchProduct } from '../reducers/product';
 import { Button } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const StyledScanBarcode = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+`;
 
 export const ScanBarcode = () => {
   const [showScanner, setShowScanner] = useState(false);
   const dispatch = useDispatch();
 
   return (
-    <>
+    <StyledScanBarcode>
       {!showScanner && (
         <Button type="button" onClick={() => setShowScanner(true)}>
           Show scanner
@@ -25,6 +34,6 @@ export const ScanBarcode = () => {
           }}
         />
       )}
-    </>
+    </StyledScanBarcode>
   );
 };
