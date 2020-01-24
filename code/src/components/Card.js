99f75da91af3bab96be6e3815076a6fdee5c00ac
Johanna Luks Wærn <johanna@misswaern.se>
2020-01-24 14:01:56 +0100

    cleaning the syntax

--- code/src/components/Card.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Card = () => {
-  return <></>
-}
-
-//kort på statistik, kort med länk till återvinningsstation, kort med något mer?
--- code/src/components/Product.js
+++ code/src/components/Product.js
@@ -5,30 +5,28 @@ export const Product = () => {
   const product = useSelector((state) => state.products.product)
   const isScanning = useSelector((state) => state.ui.isScanning)
 
-  if (!isScanning && product.status === 0) {
-    return (
-      <>
-        <p>Oh no, I couldn't find this product.</p>
-        <p>
-          If you have the time, please visit world.openfoodfacts.org and add the
-          product for others to find!
-        </p>
-      </>
-    )
-  }
+  return (
+    <div>
+      {!isScanning && product.status === 0 && (
+        <div className='not-found'>
+          <p>Oh no, I couldn't find this product.</p>
+          <p>
+            If you have the time, please visit world.openfoodfacts.org and add
+            the product for others to find!
+          </p>
+        </div>
+      )}
 
-  if (!isScanning && product.status === 1) {
-    return (
-      <>
-        <h2>{product.product.product_name}</h2>
-        <h3>
-          Please sort this as{''} {product.product.packaging.toLowerCase()}
-        </h3>
-
-        <img className='product-image' src={product.product.image_url} />
-      </>
-    )
-  }
-
-  return <></>
+      {!isScanning && product.status === 1 && (
+        <div>
+          <h2>{product.product.product_name}</h2>
+          <h3>
+            Please sort this as{''} {product.product.packaging.toLowerCase()}
+          </h3>
+          <img className='product-image' src={product.product.image_url} />
+        </div>
+      )}
+      {<></>}
+    </div>
+  )
 }
