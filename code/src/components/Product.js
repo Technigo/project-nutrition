703efc7576e98014b8b3dcf44ef9581bfad796dd
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-01-24 12:29:11 +0100

    styling of Product.js

--- code/src/components/Product.js
+++ code/src/components/Product.js
@@ -1,58 +1,57 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import styled from "styled-components"
-
+//this component Product, whill show the product when we fetched it
+import styled from 'styled-components'
 const ProductHeading = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+    display: flex;
+    flex-direction: row;
+    // width: 500px;
+    justify-content: center;
 `
 const ProductInfo = styled.div`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    // width: 500px;
     justify-content: center;
-    justify-content: space-between;
 `
-
-// const ProductPic = styled.div`
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: center;
-// `
-
+const ProductStore = styled.div`
+    display: flex;
+    flex-direction: row;
+    // width: 500px;
+    justify-content: center;
+`
+const ProductImg = styled.div`
+    display: flex;
+    flex-direction: row;
+    // width: 500px;
+    justify-content: center;
+`
 //this component Product, whill show the product when we fetched it
 export const Product = () => {
     const product = useSelector(state => state.products.product)
-    
     // if we dont have a product at all, we dont want to show anything, return null
     if (!product) return null
-
     // if we get a product, we want to show it
     console.log('product', product)
-
     return (
         <>
             <ProductHeading>
-                <img src={product.product && product.product.image_thumb_url} alt= "" />  
-                <h1>{product.product && product.product.product_name} </h1>
+                <img src={product.product && product.product.image_thumb_url} alt="" />
+                <h1>{product.product && product.product.product_name}</h1>
+                {/* <h4>Ingredients:{product.product && product.product.ingredients_tags}</h4> */}
             </ProductHeading>
-           
             <ProductInfo>
-                <h4>
-                    {product.product && product.product.ingredients_tags}
-                </h4>
-                <h5>
-                    {product.product && product.product.stores}
-                </h5>
+                <h4>Ingredients:</h4>
+                <h4>{product.product && product.product.ingredients_tags}</h4>
             </ProductInfo>
-
-           {/* <ProductPic> */}
+            <ProductStore>
+                <h5 className="Stores">Stores:{product.product && product.product.stores}</h5>
+            </ProductStore>
+            <ProductImg>
                 {/* first checking that we get the product in the fetch && then show the picture of the product */}
-                <img src={product.product && product.product.image_url} alt= "" />
-                <img src={product.product && product.product.image_ingredients_url} alt= "" />
-                <img src={product.product && product.product.image_nutrition_url} alt= "" />
-                
-           {/* </ProductPic> */}
+                <img src={product.product && product.product.image_url} alt="" />
+                <img src={product.product && product.product.image_nutrition_url} alt="" />
+            </ProductImg>
         </>
     )
 }
