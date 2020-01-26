07e304b05c7ec0528a827c48635cc47f1b5be556
sarah wolfson <swolfson23@gmail.com>
2020-01-26 14:32:16 +0100

    camera comes up

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Provider } from "react-redux"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import { ScanBarcode } from 'components/ScanBarcode'
+import { ui } from "reducers/ui"
+import { products } from "reducers/products"
+
+const reducer = combineReducers({
+  ui: ui.reducer,
+  products: products.reducer
+})
+
+export const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <ScanBarcode />
+    </Provider>
+
   )
 }
---
+++ code/src/reducers/products.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const products = createSlice({
+  name: 'products',
+  initialState: {
+    product: [],
+  },
+  reducers: {
+    setProduct: (state, action) => {
+      state.product = action.payload
+
+    }
+  }
+})
