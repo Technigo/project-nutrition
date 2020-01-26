b8c1f30ea52473294100d0084f76e4a735f0ff30
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-01-26 23:14:47 +0100

    Update style

--- code/src/components/ShowInfo.js
+++ code/src/components/ShowInfo.js
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 
 const StyledShowInfo = styled.div`
-  padding: 4rem 3rem;
+  padding: 3rem 3rem;
+`;
+
+const List = styled.ul`
+  border: 1px solid rgba(14, 30, 37, 0.13);
+  border-radius: 6px;
+  box-shadow: 0 2px 4px 0 rgba(14, 30, 37, 0.12);
+  padding: 1rem;
 `;
 
 const Item = styled.li`
@@ -31,8 +38,8 @@ export const ShowInfo = () => {
     <StyledShowInfo>
       {!showScanner && productFound.status && data && (
         <>
-          <h3>Product summary</h3>
-          <ul>
+          {/* <h3>Product summary</h3> */}
+          <List>
             <Item>
               <ItemTitle>Barcode</ItemTitle>
               {data && data.id ? (
@@ -57,7 +64,7 @@ export const ShowInfo = () => {
                 <ItemDetails>-</ItemDetails>
               )}
             </Item>
-          </ul>
+          </List>
         </>
       )}
       {!showScanner && !productFound.status && productFound.length !== 0 && (
