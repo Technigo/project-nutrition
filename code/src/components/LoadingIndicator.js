2046c51991a983f98e3f88aad9640a000dc620af
Jenfi <jennifer_fick@hotmail.com>
2020-01-26 13:37:28 +0100

    cleaning

--- code/src/components/LoadingIndicator.js
+++ code/src/components/LoadingIndicator.js
@@ -12,16 +12,9 @@ export const LoadingIndicator = () => {
 
         <div class="lds-dual-ring"></div>
 
-        // <lottie-player id="loading"
-        //   src="https://assets4.lottiefiles.com/datafiles/KS8ZpEdYbIuaZqg/data.json" background="transparent" speed="1" loop autoplay >
-        // </lottie-player>
       )}
 
     </>
 
   )
 }
-
-
-{/* // Kolla ca 59 min in för detta, tidigare för att ladda ner loading-indicator */ }
-{/* // <Loader type="" color="" widht={} height={} /> */ }
--- code/src/components/ScanBarcode.js
+++ code/src/components/ScanBarcode.js
@@ -8,45 +8,32 @@ import './scanner.css'
 
 export const ScanBarcode = () => {
   const [showScanner, setShowScanner] = useState(false)
-  const dispatch  = useDispatch()
-  const product = useSelector(state => state.products.product) 
+  const dispatch = useDispatch()
+  const product = useSelector(state => state.products.product)
 
-  
+
   return (
     <>
-      {!showScanner && 
-      // !product.product &&
-      (
-<>
-    {/* <div className="scan-container">
-      <div className="scan-placeholder">
-        <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_9FZkDo.json"  background="transparent"  speed="0.5"  loop autoplay ></lottie-player>
-      </div>
-      </div> */}
-
+      {!showScanner &&
         <button type="button" onClick={() => setShowScanner(true)}>
           Show scanner
         </button>
         </>
-      )}
+  )
+}
 
-      {showScanner && (
-        <>
-        <BarcodeScanner className="scanner" onDetected={(code) => {
-          console.log('Got barcode', code)
-          setShowScanner(false)
-          dispatch(fetchProduct(code))
-        }} />
-        <button type="button" id="cancel-button" onClick={() => setShowScanner(false)}>Cancel</button>
-        </>
-      )}
-
-      {/* {!showScanner && product.product &&(
- <button type="button" onClick={() => setShowScanner(true)}>
- Scan again!
-</button> */}
-
-      {/* )} */}
+{
+  showScanner && (
+    <>
+      <BarcodeScanner className="scanner" onDetected={(code) => {
+        console.log('Got barcode', code)
+        setShowScanner(false)
+        dispatch(fetchProduct(code))
+      }} />
+      <button type="button" id="cancel-button" onClick={() => setShowScanner(false)}>Cancel</button>
+    </>
+  )
+}
     </>
   )
 }
