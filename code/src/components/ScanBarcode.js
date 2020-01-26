e7f3f4db7cf7d6dfc18e7097ad64abedc389b148
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-01-26 23:21:48 +0100

    Update structure

--- code/src/components/ScanBarcode.js
+++ code/src/components/ScanBarcode.js
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchProduct } from '../reducers/food';
 import { scanner } from 'reducers/scanner';
 import styled from 'styled-components';
-// import './barcodeScanner.css';
 
 const StyledScanBarcode = styled.div`
   width: 100%;
@@ -14,8 +13,6 @@ const StyledScanBarcode = styled.div`
   align-items: center;
   margin-top: 30px;
 `;
-
-// className="scanner"
 
 export const ScanBarcode = () => {
   const showScanner = useSelector(state => state.scanner.showScanner);
