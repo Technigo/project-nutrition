dd1a961ba14ba6491dfe1fa1dca209ab3a0f2db6
nasimmhn <nasim.mahzoun@gmail.com>
2020-01-27 23:06:27 +0100

    responsivness adjustments

--- code/src/App.js
+++ code/src/App.js
@@ -25,7 +25,7 @@ export const App = () => {
 
   return (
     <Provider store={store}>
-
+      <div />
       <main>
         <Router>
           <Switch>
--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -6,9 +6,7 @@ import './css/footer.css'
 export const Footer = () => {
   return (
     <div className="footer-container">
-      <h4>About this site!</h4>
-      <p>The data is taken from Open Food Facts, a free and open database. If you have some issues with scanning or finding products,
-         please visit <a href="https://world.openfoodfacts.org/" target="_blank">Open Food Facts</a></p>
+      <p>Data from Open Food Facts, a free and open database. <a href="https://world.openfoodfacts.org/" target="_blank">Open Food Facts</a></p>
     </div>
   )
 }
