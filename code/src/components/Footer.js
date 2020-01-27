42eb9a4c631dfe468be297626b0f9c33e714e44e
nasimmhn <nasim.mahzoun@gmail.com>
2020-01-27 23:34:13 +0100

    cleaned some code

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -7,6 +7,7 @@ export const Footer = () => {
   return (
     <div className="footer-container">
       <p>Data from Open Food Facts, a free and open database. <a href="https://world.openfoodfacts.org/" target="_blank">Open Food Facts</a></p>
+      <p>Nasim Mahzoun &copy; 2020</p>
     </div>
   )
 }
