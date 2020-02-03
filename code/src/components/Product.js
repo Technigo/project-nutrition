ae016215fc68c096e35fefe5b6dcea8a9d5a3ad0
Ivett Backlund <ivett.backlund@gmail.com>
2020-02-03 22:20:35 +0100

    first version without styling

--- code/src/components/Product.js
+++ code/src/components/Product.js
@@ -2,17 +2,22 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 export const Product = () => {
-    const product = useSelector(state => state.products.product)
+    const productScan = useSelector(state => state.products.product)
 
-    if (!product)
+    if (!productScan)
         return null
 
-    console.log('product', product)
+    console.log('product', productScan)
 
     return (
         <>
-            <h1>Product:</h1>
-            <img src={product.product && product.product.image_front_thumb_url} alt="product" />
+            {productScan.product && (
+                <section>
+                    <h1>{productScan.product.product_name}</h1>
+                    <h3>{productScan.product.code}</h3>
+                    <img src={productScan.product.image_front_thumb_url} alt="product" />
+                </section>
+            )}
         </>
     )
 }
