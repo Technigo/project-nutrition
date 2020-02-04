b01eaebde69fe790aec8e427bc1dd7083d2f108c
Krasnal <cislowski.karol@hotmail.com>
2020-02-04 14:13:28 +0100

    style

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -21,6 +21,12 @@ const Message = styled.p`
   font-size: 1.5rem;
 `
 
+const Button = styled.button`
+  padding: 10px 30px;
+  background-color: #df3457;
+  color: #fff;
+`
+
 export const Header = () => {
   const dispatch = useDispatch()
   const showScanner = useSelector((state) => state.scanner.showScanner)
@@ -31,11 +37,11 @@ export const Header = () => {
         Scan the barcode and check macro of 100g/ml of product
       </Message>
       {!showScanner && (
-        <button
+        <Button
           type="button"
           onClick={() => dispatch(scanner.actions.setShowScanner(true))}>
           Scan product
-        </button>
+        </Button>
       )}
     </HeaderStyled>
   )
