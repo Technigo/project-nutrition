a5ada8e7bc69ecd6ee8ddf770f24d3d89af8912c
sarah wolfson <swolfson23@gmail.com>
2020-03-31 16:16:31 +0200

    fixed styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import { Provider } from "react-redux"
-import { configureStore, combineReducers } from "@reduxjs/toolkit"
-import { ScanBarcode } from 'components/ScanBarcode'
-import { ui } from "reducers/ui"
-import { products } from "reducers/products"
-import { LoadingIndicator } from 'components/LoadingIndicator'
-import { Product } from 'components/Product'
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { ScanBarcode } from "components/ScanBarcode";
+import { ui } from "reducers/ui";
+import { products } from "reducers/products";
+import { LoadingIndicator } from "components/LoadingIndicator";
+import { Product } from "components/Product";
 
 const reducer = combineReducers({
   ui: ui.reducer,
   products: products.reducer
-})
+});
 
-export const store = configureStore({ reducer })
+export const store = configureStore({ reducer });
 
 export const App = () => {
   return (
     <Provider store={store}>
       <div className="roots">
         <LoadingIndicator />
-        <ScanBarcode />
-        <Product />
+        <div className="infoContainer">
+          <ScanBarcode />
+          <Product />
+        </div>
       </div>
     </Provider>
-
-
-
-  )
-}
+  );
+};
--- code/src/components/Product.js
+++ code/src/components/Product.js
@@ -1,51 +1,53 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import './product.css'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import "./product.css";
 
 export const Product = () => {
-  const scan = useSelector(state => state.products.product)
-  let formatIngredients = []
-  let formatNutritients = []
-  console.log('scan', scan)
+  const scan = useSelector(state => state.products.product);
+  let formatIngredients = [];
+  let formatNutritients = [];
+  console.log("scan", scan);
 
-
-
-
-
-  if (!scan) return null
+  if (!scan) return null;
 
   if (scan.product && scan.product.ingredients_analysis_tags) {
     formatIngredients = scan.product.ingredients_analysis_tags.map(ingredient =>
-      ingredient.replace(/\w+:/, ' ').replace(/-/gi, ' ').replace(':', ' ')
-    )
+      ingredient
+        .replace(/\w+:/, " ")
+        .replace(/-/gi, " ")
+        .replace(":", " ")
+    );
   }
 
   if (scan.product && scan.product.nutrient_levels_tags) {
     formatNutritients = scan.product.nutrient_levels_tags.map(ingredient =>
-      ingredient.replace(/\w+:/, ' ').replace(/-/gi, ' ').replace(':', ' ')
-    )
+      ingredient
+        .replace(/\w+:/, " ")
+        .replace(/-/gi, " ")
+        .replace(":", " ")
+    );
   }
 
   return (
     <div className="container">
-
-      {!scan.product &&
+      {!scan.product && (
         <div className="intro-container">
           <div className="intro-text">
             <h1> Healthy Life </h1>
             <h2>Ingredient and Nutritional information</h2>
           </div>
         </div>
-      }
+      )}
 
-      {scan.product &&
+      {scan.product && (
         <div className="card">
           <img src={scan.product.image_front_url} alt="productpic" />
-          <img src={scan.product.image_ingredients_url} alt="ingredients pics" />
+          <img
+            src={scan.product.image_ingredients_url}
+            alt="ingredients pics"
+          />
 
           <div>
-
             <ul className="product">
               <span className="highlight-info">Ingredients:</span>
               {formatIngredients.map((ingredient, index) => (
@@ -61,16 +63,12 @@ export const Product = () => {
             </ul>
           </div>
         </div>
-      }
-      {scan.status === 0 &&
-
+      )}
+      {scan.status === 0 && (
         <div className="not-found">
-          <h1 >
-            Please try again or chocie another product!
-          </h1>
+          <h1>Please try again or choice another product!</h1>
         </div>
-
-      }
+      )}
     </div>
-  )
-}
+  );
+};
