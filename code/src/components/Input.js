eb4b7bce2e713ce37ebbc8db8f24b5e6790675c1
palhamel <pal.hamel@gmail.com>
2020-04-24 15:42:48 +0200

    Added hardcoded product input

--- code/src/components/Input.js
+++ code/src/components/Input.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from 'react-redux'
+import { productsList } from "reducers/products";
+import { fetchProducts } from 'reducers/products'
 
-export const Input = () => {
+export const Input = (e) => {
+  const dispatch = useDispatch()
+
+  const handleOnSubmit = () => {
+    dispatch(productsList.actions.fetchProducts())
+  }
+
+  // const handleOnSubmit = (event) => {
+  //   return (
+  //     event.preventDefault()
+  //     dispatch(products.productsList.actions.fetchProducts())
+  //   )
+  // }
+
+  // const onDetected = (code) => {
+  //   console.log(`Code: ${code}`);
+  //   fetch(`https://world.openfoodfacts.org/api/v0/product/${code}.json`)
+  //     .then((data) => data.json())
+  //     .then((json) => {
+  //       console.log(json);
+  //     });
+  // };
+
   return (
-    
-  )
-}
+    <div>
+      <label>
+        {" "}
+        Test codes here:{" "}
+        {/* <input type="text" value={(e.target.value)}></input> */}
+        <input type="text"></input>
+        <button type="button" onClick={() => dispatch(fetchProducts(7310130003530))}>Submit the Snabbmakaroner</button>
+      </label>
+      <p>
+        {" "}
+        Use the field above to test barcodes manually and keep an eye on your
+        console in the browser. i.e. Type 7311070347272 - Pågen Gifflar. Yum
+        Pasta: 7310130003530 , Kikärtor: 7340011487906
+      </p>
+      {/* <BarcodeScanner /> */}
+    </div>
+  );
+};
