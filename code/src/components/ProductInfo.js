86c554618bc72b58c1b2dbecc801777b19ad4f10
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-04-28 20:02:36 +0200

    Added some comments + cleaned up

--- code/src/components/ProductInfo.js
+++ code/src/components/ProductInfo.js
@@ -7,34 +7,41 @@ import { ScanBarcode } from './ScanBarcode'
 export const ProductInfo = () => {
   const scannedProduct = useSelector((store) => store.product.product)
 
-  if (scannedProduct.status === 1) {
+  if (scannedProduct.status === 1) { // Show if product was found
+
     const allergens = scannedProduct.product.allergens_tags
-    const allergensExists = allergens.length > 0
+    const allergensExists = allergens.length > 0 // Check if product has allergen tags
 
     return (
       <Wrapper>
         <Title>{scannedProduct.product.product_name}</Title>
+
+        {/* Show if product has allergen tags */}
         {allergensExists ?
-          <SubTitle> This product contains the following allergens:
+          <>
+            <SubTitle> This product contains the following allergens:</SubTitle>
             <ul>
-              {allergens.map(allergen =>
-                <li key={allergen}>{
-                  allergen
+              {allergens.map(allergen => // Map through allergen tags
+                <li key={allergen}>
+                  {allergen
                     .replace(/\w+:/, " ")
                     .replace(/-/gi, " ")
-                    .replace(":", " ")
-                }</li>
+                    .replace(":", " ")}
+                </li>
               )}
             </ul>
-          </SubTitle>
+          </>
+
           :
+
           <SubTitle>Contains no allergens!</SubTitle>
+
         }
+
         <ScanBarcode />
       </Wrapper>
     )
-  } else if (scannedProduct.status === 0) {
-
+  } else if (scannedProduct.status === 0) { // If product was not found
     return (
       <Wrapper>
         <Title>Sorry, no product found!</Title>
@@ -46,26 +53,26 @@ export const ProductInfo = () => {
       <ScanBarcode />
     )
   }
-};
+}
 
 const Wrapper = styled.section`
-display: flex;
-justify-content: center;
-flex-direction: column;
-max-width: 800px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 800px;
 `
 
 const Title = styled.h2`
-margin: 25px auto;
-font-size: 26px;
-text-align: center;
-max-width: 85%;
+  margin: 25px auto;
+  font-size: 26px;
+  text-align: center;
+  max-width: 85%;
 `
 
 const SubTitle = styled.h3`
-max-width: 85%;
-margin: 0;
-font-size: 24px;
-text-align: center;
-margin: auto;
+  max-width: 85%;
+  margin: 0;
+  font-size: 24px;
+  text-align: center;
+  margin: auto;
 `
