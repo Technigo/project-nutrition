dca4e2e553366171cc6c3cc28d9bd1ae86a0128c
Josephine <josephine@Josephines-Air.lan>
2020-04-30 18:43:46 +0200

    deleted unecessary comments and changed welcome text

--- code/src/App.js
+++ code/src/App.js
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Provider, useDispatch } from 'react-redux';
-import { BarcodeScanner } from 'components/BarcodeScanner';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { products } from 'reducers/products';
 import { ScannedProduct } from 'components/Scannedproduct';
 import { Input } from './components/Input';
 import { CameraButton } from 'components/CameraButton';
-import { startPage } from 'components/StartPage';
-
-// Fixa thunk
-// set up store
-// reducers
-// spara input i en state - för att kunna skicka in state i onDetected
-// component: input
-// component: isvegan
-// component: ingredients analysis (gluten, palmoil, vegan)
 
 const reducer = combineReducers({ products: products.reducer });
 
