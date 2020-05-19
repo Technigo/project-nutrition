ef36f62da0d0e787e92267b0004d8fe417ec7d3d
camilla <camilla.gausland@gmail.com>
2020-05-19 18:43:57 +0200

    status code

--- code/src/components/NutritionList.js
+++ code/src/components/NutritionList.js
@@ -9,19 +9,27 @@ export const NutritionList = () => {
     <>
     
       <div>
-       {product.product && (
+      {product.status === 0 ? <h2>No product found</h2> :  
+
+       product.product && (
          <div className='nutrition-card'> 
-          {product.product && (<h2 className='product-name'>{product.product.product_name}</h2>)}   
+          {product.product && (<h2 className='product-name'>{product.product.product_name}</h2>)}  
+          
             <ul className='title-and-pic'>
               {product.product && (<li><img src={product.product.image_front_url} alt='pic'/> </li>  )}   
               {product.product && (<li><span className='title'>Ingredients</span></li>)} 
             </ul>
             <ul className='ingredient-list'>
-              {product.product.ingredients_tags && product.product.ingredients_tags.map((ingredient) => 
-               <li> { ingredient.replace(/\w+:/, '').replace('-', ' ')} </li> )} 
+              <li> 
+              {product.product.ingredients_tags.length > 0 ? product.product.ingredients_tags.map((ingredient) => 
+               ( ingredient.replace(/\w+:/, '').replace('-', ' '))) : `No ingredients found` } 
+           </li> 
             </ul>
+            <div>
+             
+            </div>
           </div>
-       )}
+       )} 
       </div>
     
     </>
