95fbf5b31220c933d03481cac2755aa8441958b9
camilla <camilla.gausland@gmail.com>
2020-05-25 10:11:50 +0200

    no product found, with nice styling

--- code/src/components/NutritionList.js
+++ code/src/components/NutritionList.js
@@ -5,33 +5,30 @@ export const NutritionList = () => {
    const product = useSelector((state) => state.foods.foodProduct)
 
 
-  return (
+   return (
     <>
-    
+   {product.status === 0 && (
+     <div className='nutrition-card'>
+       <h2>No product found</h2>
+       </div>
+   )}
+    {product.product && product.status === 1 && (
+      <div className='nutrition-card'>
+    <h2 className='product-name'>{product.product.product_name}</h2>  
+      <ul className='title-and-pic'>
+        {product.product && (<li><img src={product.product.image_front_url} alt='pic'/> </li>  )}   
+        {product.product && (<li><span className='title'>Ingredients</span></li>)} 
+      </ul>
+      <ul className='ingredient-list'>
+        {product.product.ingredients_tags && product.product.ingredients_tags.length > 0 ? product.product.ingredients_tags.map((ingredient) => 
+         (
+           <li>{ingredient.replace(/\w+:/, '').replace('-', ' ')}</li>))
+           : <li>No ingredients found</li>  } 
+      </ul>
+      </div> 
+      )}
       <div>
-      {product.status === 0 ? <h2>No product found</h2> :  
-
-       product.product && (
-         <div className='nutrition-card'> 
-          {product.product && (<h2 className='product-name'>{product.product.product_name}</h2>)}  
-          
-            <ul className='title-and-pic'>
-              {product.product && (<li><img src={product.product.image_front_url} alt='pic'/> </li>  )}   
-              {product.product && (<li><span className='title'>Ingredients</span></li>)} 
-            </ul>
-            <ul className='ingredient-list'>
-              <li> 
-              {product.product.ingredients_tags.length > 0 ? product.product.ingredients_tags.map((ingredient) => 
-               ( ingredient.replace(/\w+:/, '').replace('-', ' '))) : `No ingredients found` } 
-           </li> 
-            </ul>
-            <div>
-             
-            </div>
-          </div>
-       )} 
       </div>
-    
-    </>
-  )
+      </>
+)
 }
