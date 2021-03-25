9614bdd6818960e9f553798c487e530873545d62
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2021-03-25 11:43:21 +0100

    Update App.js

--- code/src/App.js
+++ code/src/App.js
@@ -6,7 +6,7 @@ const onDetected = (code) => {
   fetch(`https://world.openfoodfacts.org/api/v0/product/${code}.json`)
     .then((data) => data.json())
     .then((json) => {
-      // console.log(json);
+      console.log(json);
     });
 };
 
@@ -16,14 +16,14 @@ export const App = () => {
       <label>
         {" "}
         Test codes here:{" "}
-        <input type="text" onChange={(e) => onDetected(e.target.value)}></input>
+        <input type="text" onChange={(e) => onDetected(e.target.value)} />
       </label>
       <p>
         {" "}
         Use the field above to test barcodes manually and keep an eye on your
         console in the browser. i.e. Type 7311070347272 - Pågen Gifflar. Yum
       </p>
-      <BarcodeScanner onDetected={onDetected}></BarcodeScanner>
+      <BarcodeScanner onDetected={onDetected} />
     </div>
   );
 };
